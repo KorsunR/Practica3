0696495eeabb7939ca5d024a7a88a00e93739dcd
KorsunR <rkorsun57@gmail.com>
2023-03-22 09:02:49 +0300

    JJJJ

---
+++ practica1pr/FirstPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using practica1pr.AORUSDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace practica1pr
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstPage.xaml
+    /// </summary>
+    public partial class FirstPage : Page
+    {
+        ColorTableAdapter color = new ColorTableAdapter(); 
+        public FirstPage()
+        {
+            InitializeComponent();
+            ColorDataGrid.ItemsSource = color.GetData();
+            ColorDataGrid.DisplayMemberPath = "color_name";
+        }
+    }
+}
---
+++ practica1pr/SecondPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using practica1pr.AORUSDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace practica1pr
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondPage.xaml
+    /// </summary>
+    public partial class SecondPage : Page
+    {
+        PickturesTableAdapter pictures = new PickturesTableAdapter();
+        public SecondPage()
+        {
+            InitializeComponent();
+            ColorDataGrid.ItemsSource = pictures.GetData();
+            ColorDataGrid.DisplayMemberPath = "picktures_name";
+        }
+    }
+}
